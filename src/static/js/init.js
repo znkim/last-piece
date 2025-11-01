ab32e0f6ee2caa2287f6ddb0eae93d17a5b4aa4d
znkim <znkim@kakao.com>
2025-11-02 00:55:58 +0900

    build: fix link for mobile

--- src/static/js/init.js
+++ src/static/js/init.js
@@ -1,6 +1,6 @@
-const isMobile = /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
-const BASE_URL = new URL('.', import.meta.url).pathname
+const ROOT = new URL('.', location).pathname;
+let isMobile = /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
 if (isMobile) {
   console.log("Redirecting to mobile version");
-  window.location.href = BASE_URL + "/mobile";
+  window.location.href = ROOT + "/mobile";
 }
